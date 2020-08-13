9f9f71ea7ad396a8a6cd7c13009f4df5ad8630df
C1DeChand <C1DeChand@gmail.com>
2020-08-13 13:23:19 -0700

    Updates

---
+++ precache-manifest.1ebee0d2f9e091add74434d3dffa4833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbfe2257a78c73bb8e82b5464a2361f",
+    "url": "/BC-Homework19/index.html"
+  },
+  {
+    "revision": "0ac441113e56a8401e0f",
+    "url": "/BC-Homework19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6eeba199a98cf58ed09f",
+    "url": "/BC-Homework19/static/js/2.a244c3f5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BC-Homework19/static/js/2.a244c3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac441113e56a8401e0f",
+    "url": "/BC-Homework19/static/js/main.37388276.chunk.js"
+  },
+  {
+    "revision": "1cb45337304b6c5579fd",
+    "url": "/BC-Homework19/static/js/runtime-main.98bdadab.js"
+  }
+]);
